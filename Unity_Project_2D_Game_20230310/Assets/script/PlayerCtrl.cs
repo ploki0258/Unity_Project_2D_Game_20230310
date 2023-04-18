d109e3b66f311f2bd522891ff4c8231dbb3b4668
ploki0258 <57485339+ploki0258@users.noreply.github.com>
2023-04-18 18:09:56 +0800

    新增角色&對話程式

--- Unity_Project_2D_Game_20230310/Assets/script/PlayerCtrl.cs
+++ Unity_Project_2D_Game_20230310/Assets/script/PlayerCtrl.cs
@@ -1,18 +1,130 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 在整個專案全域宣告一個instance(讓PlayerCtrl變成單例)
+    public static PlayerCtrl instance = null;
+
+    [Header("移動速度")]
+    [SerializeField] float speed = 1.5f;
+    [Header("最大血量")]
+    [SerializeField] float hpMax = 100;
+    [Header("血條")]
+    [SerializeField] Image hpBar = null;
+    [Header("血量數值")]
+    [SerializeField] Text hpText = null;
+    [Header("時間文字介面")]
+    [SerializeField] Text textTime = null;
+
+    [SerializeField] Slider 體力條 = null;
+
+    Collider2D tempTarget = null;
+    bool startTimer = false;
+    private Rigidbody2D rig = null;
+    private Animator ani = null;
+
+    private void Awake()
     {
-        
+        instance = this;    // 讓單例等於自己
+        rig = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        hp = hpMax;
+    }
+
+    private void Update()
+    {
+        Move();
+        timer();
+    }
+
+    /// <summary>
+    /// 角色移動：動畫、翻轉
+    /// </summary>
+    private void Move()
+    {
+        float ws = Input.GetAxisRaw("Vertical");    // 垂直 -1~1
+        float ad = Input.GetAxisRaw("Horizontal");  // 水平 -1~1
+
+        Vector2 move = new Vector2(ad * speed, ws * speed);
+        rig.velocity = move;           // 角色移動
+        // transform.Translate(move);  // 角色移動(需在乘上Time.deltaTime)
+
+        // 角色動畫
+        /*
+        if (move != Vector2.zero)
+        {
+            ani.SetBool("向上走", ws > 0);
+            ani.SetBool("向下走", ws < 0);
+            ani.SetBool("向右走", ad > 0);
+        }
+        */
+        //翻轉
+        /*
+        if (move.x < 0)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+        */
+    }
+
+    /// <summary>
+    /// 血量控制
+    /// </summary>
+    float hp
+    {
+        get { return hpMax * hpBar.fillAmount; } // 讀取
+        set // 寫入
+        {
+            _hp = value;
+            hpBar.fillAmount = value / hpMax;   // 百分比
+            hpText.text = Mathf.Round(hp) + "/" + hpMax;
+            體力條.value = value / hpMax;
+        }
+    }
+    float _hp = 0f;
+
+    /// <summary>
+    /// 傷害管理器
+    /// </summary>
+    /// <param name="hurt">所受的傷害量</param>
+    public void TakeDamage(float hurt)
+    {
+        hp -= hurt;
+    }
+
+    /// <summary>
+    /// 更新時間介面
+    /// </summary>
+    private void timer()
+    {
+        if (tempTarget != null && startTimer == true && hp != 0)
+        {
+            textTime.text = "時間：" + Time.time; // 時間文字介面 = "時間：" + 載入場景時間(當下載入場景經過多少時間)
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Floor")
+        {
+            tempTarget = collision;
+        }
+        startTimer = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Floor")
+        {
+            tempTarget = null;
+        }
+        startTimer = false;
     }
 }
